9fea08806a294f4ef84f7eca0c836a1dad684f10
Evo Eclipse <evo.galaio@outlook.com>
2025-09-05 18:20:21 +0300

    Initial Commit

---
+++ TravelSchedules/TravelSchedulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelSchedulesApp.swift
+//  TravelSchedules
+//
+//  Created by Pavel Komarov on 05.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelSchedulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
